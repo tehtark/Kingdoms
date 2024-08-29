073aa3f90daa1838ae5b661c44642a6c9492c92b
Tark <41029975+tehtark@users.noreply.github.com>
2024-08-30 00:10:36 +0100

    added a basic inventory to the added to the player entity

---
+++ Kingdoms.Server.Domain/Entities/Player/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using Kingdoms.Server.Domain.Entities.Items;
+
+namespace Kingdoms.Server.Domain.Entities.Player;
+
+public class Inventory : List<Item>
+{
+    public int Id { get; set; }
+    public int Gold { get; private set; }
+
+    public Inventory(int id, int gold)
+    {
+        Id = id;
+        Gold = gold;
+    }
+
+    public void AddGold(int amount)
+    {
+        Gold += amount;
+    }
+
+    public void RemoveGold(int amount)
+    {
+        Gold -= amount;
+    }
+}
