3f10b889ab92f2c9a968717acf56da7f4a71a8cd
Tark <connorjamessmith@gmail.com>
2024-08-30 00:50:17 +0100

    added properties to the item class, name and description, implemented the constructor for the class too

--- Kingdoms.Server.Domain/Entities/Items/Item.cs
+++ Kingdoms.Server.Domain/Entities/Items/Item.cs
@@ -3,5 +3,15 @@
 public class Item
 {
     public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
     public int Quantity { get; set; }
+
+    public Item(int id, string name, string description, int quantity)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Quantity = quantity;
+    }
 }
