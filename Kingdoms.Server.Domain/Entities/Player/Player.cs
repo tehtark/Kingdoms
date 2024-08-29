099c8296bebd0a7487d3babb10a8acb6cdf6895c
Tark <41029975+tehtark@users.noreply.github.com>
2024-08-30 00:10:49 +0100

    created a basic player entity.

---
+++ Kingdoms.Server.Domain/Entities/Player/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Kingdoms.Server.Domain.Entities.Player;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public int Level { get; set; }
+    public int Experience { get; set; }
+    public Inventory Inventory { get; set; }
+
+    public Player(int id, string username, Inventory inventory)
+    {
+        Id = id;
+        Username = username;
+        Inventory = inventory;
+    }
+}
