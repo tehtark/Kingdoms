0e87bc7145768066ba4853c2a10c0dc5426ebbb7
Tark <connorjamessmith@gmail.com>
2024-09-03 01:47:36 +0100

    Implemented a GameHubService to connect to the API Hub

---
+++ Kingdoms.Client.Application/Services/GameHubService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Kingdoms.Client.Application.Services;
+
+internal class GameHubService
+{
+    private NavigationManager _navigationManager;
+    private HubConnection _hubConnection;
+
+    public GameHubService(NavigationManager navigationManager)
+    {
+        _navigationManager = navigationManager;
+    }
+
+    public async Task ConnectAsync()
+    {
+        _hubConnection = new HubConnectionBuilder()
+            .WithUrl(_navigationManager.ToAbsoluteUri("/gamehub")) // Use NavigationManager
+            .Build();
+
+        // Add event handlers for connection events (optional)
+        _hubConnection.Closed += async (error) => {
+            // Handle connection closed (e.g., attempt to reconnect)
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await _hubConnection.StartAsync();
+        };
+
+        await _hubConnection.StartAsync();
+    }
+}
