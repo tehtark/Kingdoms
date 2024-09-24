ee8d33107f45f7465261dfda11d1a000fe626c70
Tark <connorjamessmith@gmail.com>
2024-09-25 00:55:10 +0100

    updated geojson data file location

--- Kingdoms.Application/Features/Map/DeserialiseGeoJsonCommand.cs
+++ Kingdoms.Application/Features/Map/DeserialiseGeoJsonCommand.cs
@@ -6,11 +6,11 @@ namespace Kingdoms.Application.Features.Map;
 
 public class DeserialiseGeoJsonCommand : IRequest<FeatureCollection?>;
 
-internal class DeserialiseGeoJsonCommandHandler : IRequestHandler<DeserialiseGeoJsonCommand, FeatureCollection?>
+internal class DeserialiseGeoJsonCommandHandler(HttpClient httpClient) : IRequestHandler<DeserialiseGeoJsonCommand, FeatureCollection?>
 {
     public async Task<FeatureCollection?> Handle(DeserialiseGeoJsonCommand request, CancellationToken cancellationToken)
     {
-        var json = await File.ReadAllTextAsync(AppDomain.CurrentDomain.BaseDirectory + @"Map\toureia.geojson");
+        var json = await httpClient.GetStringAsync("https://raw.githubusercontent.com/tehtark/Kingdoms/refs/heads/dev/Kingdoms.Infrastructure/Map/toureia.geojson");
         var reader = new GeoJsonReader();
         return reader.Read<FeatureCollection>(json);
     }
